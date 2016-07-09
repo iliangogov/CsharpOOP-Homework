4fd699e08b1d5a25951a44666024eea21533be0f
iliangogov <iliangogov@gmail.com>
2016-07-09 13:27:58 +0300

    .

--- ExamPreparation/Furnitures/FurnitureManufacturer/Models/Furniture.cs
+++ ExamPreparation/Furnitures/FurnitureManufacturer/Models/Furniture.cs
@@ -43,7 +43,7 @@ namespace FurnitureManufacturer.Models
         {
             get
             {
-                return this.MaterialType.ToString();
+                return this.MaterialType;
             }
         }
 
